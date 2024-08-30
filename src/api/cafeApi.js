42b7667b579bb700bd4a88ec72d39f9f607d61a1
Neeraj824 <53514637+Neeraj824@users.noreply.github.com>
2024-08-30 21:43:50 +0800

    Update cafeApi.js

--- src/api/cafeApi.js
+++ src/api/cafeApi.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 
-const API_URL = 'http://localhost:3000/api';
+const API_URL = process.env.REACT_APP_API_URL;
 
 export const getCafes = async (location) => {
   const response = await axios.get(`${API_URL}/cafes`, { params: { location } });
